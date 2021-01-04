c9387b025f2e5a7f3b28d4eb8f98c3e140e1076e
RikiRC <47928112+RikiRC@users.noreply.github.com>
2021-01-04 18:51:53 +0100

    Update checkForNewWR.py

--- checkForNewWR.py
+++ checkForNewWR.py
@@ -1,25 +1,38 @@
 import requests, json, jsondiff
 
-url = 'https://www.speedrun.com/api/v1/games/gtavc/records?top=1&miscellaneous=yes&scope=full-game'
-headers = {
-    'User-Agent': 'Is-there-a-new-wr/0.1'
-}
-
-httpRequestForGame = requests.get(url, headers=headers)
-jsonData = json.loads(httpRequestForGame.text)
+gamesList = ['gta1','gtal69','gtal61','gta2','gtaiii','gtaa','gtasa','gtalcs','gtavcs','gtaiv','gtatlad','gtacw','gtatbogt','gtao','gtav']
 
 wr_database_temp = {}
 wr_database_temp['data'] = []
 
-for i in jsonData['data']:
-    for j in i['runs']:
-        wr = {
-            'game': j['run']['game'],
-            'category': j['run']['category'],
-            'player': j['run']['players'][0]['id'],
-            'id': j['run']['id']            
-        }
-        wr_database_temp['data'].append(wr)
+for i in gamesList:
+    url = 'https://www.speedrun.com/api/v1/games/'+i+'/records?top=1&miscellaneous=yes&scope=full-game'
+    headers = {
+        'User-Agent': 'Is-there-a-new-wr/0.2'
+    }
+    httpRequestForGame = requests.get(url, headers=headers)
+    jsonData = json.loads(httpRequestForGame.text)
+    for j in jsonData['data']:
+        for k in j['runs']:
+            playerType = k['run']['players'][0]['rel']
+            if playerType == 'guest':
+                wr = {
+                    'game': k['run']['game'],
+                    'category': k['run']['category'],
+                    'playerType': k['run']['players'][0]['rel'],
+                    'player': k['run']['players'][0]['name'],
+                    'id': k['run']['id']
+                }
+                wr_database_temp['data'].append(wr)
+            elif playerType == 'user':
+                wr = {
+                    'game': k['run']['game'],
+                    'category': k['run']['category'],
+                    'playerType': k['run']['players'][0]['rel'],
+                    'player': k['run']['players'][0]['id'],
+                    'id': k['run']['id']
+                }
+                wr_database_temp['data'].append(wr)
 
 with open('wrDatabase.json') as wrDatabaseJson:
     wrDatabase = json.load(wrDatabaseJson)
@@ -32,17 +45,22 @@ if checkDiff != {}:
         newWrEntry = list(filter(lambda x:x['id']==idTest,wr_database_temp['data']))
         url = 'https://www.speedrun.com/api/v1/games/'+newWrEntry[0]['game']
         gameName = requests.get(url, headers=headers)
-        jsonGameName = json.loads(gameName.text)
+        jsonGameName = json.loads(gameName.text)['data']['names']['international']
         url = 'https://www.speedrun.com/api/v1/categories/'+newWrEntry[0]['category']
         categoryName = requests.get(url, headers=headers)
-        jsonCategoryName = json.loads(categoryName.text)
-        url = 'https://www.speedrun.com/api/v1/users/'+newWrEntry[0]['player']
-        playerName = requests.get(url, headers=headers)
-        jsonPlayerName = json.loads(playerName.text)
+        jsonCategoryName = json.loads(categoryName.text)['data']['name']
         url = 'https://www.speedrun.com/api/v1/runs/'+newWrEntry[0]['id']
         runStats = requests.get(url, headers=headers)
-        jsonRunStats = json.loads(runStats.text)
-        print('New WR: ' + jsonGameName['data']['names']['international'] + ' - ' + jsonCategoryName['data']['name'] + ' ' + jsonRunStats['data']['times']['primary'].replace('PT','').replace('H','h ').replace('M','m ').replace('S','s') + ' by ' + jsonPlayerName['data']['names']['international'] + ' Congratz! More info at: ' + jsonRunStats['data']['weblink'])
+        jsonRunStats = json.loads(runStats.text)['data']['times']['primary']
+        jsonRunStats = jsonRunStats.replace('PT','').replace('H','h ').replace('M','m ').replace('S','s')
+        jsonRunStatsWeblink = json.loads(runStats.text)['data']['weblink']
+        if newWrEntry[0]['playerType'] == 'user':
+            url = 'https://www.speedrun.com/api/v1/users/'+newWrEntry[0]['player']
+            playerName = requests.get(url, headers=headers)
+            jsonPlayerName = json.loads(playerName.text)['data']['names']['international'] 
+        elif newWrEntry[0]['playerType'] == 'guest':
+            jsonPlayerName = newWrEntry[0]['player']
+        print('New WR: ' + jsonGameName + ' - ' + jsonCategoryName + ' ' + jsonRunStats + ' by ' + jsonPlayerName + ' Congratz! More info at: ' + jsonRunStatsWeblink)
     print('Saving updated WR database...')
     jsonWrDatabase = json.dumps(wr_database_temp, indent = 4)
     with open("wrDatabase.json", "w") as outfile:
