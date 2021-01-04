3f5adf294b943b121e72aebe4757aee61b8b3b6d
RikiRC <47928112+RikiRC@users.noreply.github.com>
2021-01-04 18:52:14 +0100

    Update createWRDatabase.py

--- createWRDatabase.py
+++ createWRDatabase.py
@@ -1,25 +1,38 @@
 import requests, json
 
-url = 'https://www.speedrun.com/api/v1/games/gtavc/records?top=1&miscellaneous=yes&scope=full-game'
-headers = {
-    'User-Agent': 'Is-there-a-new-wr/0.1'
-}
-
-httpRequestForGame = requests.get(url, headers=headers)
-jsonData = json.loads(httpRequestForGame.text)
+gamesList = ['gta1','gtal69','gtal61','gta2','gtaiii','gtaa','gtasa','gtalcs','gtavcs','gtaiv','gtatlad','gtacw','gtatbogt','gtao','gtav']
 
 wr_database = {}
 wr_database['data'] = []
 
-for i in jsonData['data']:
-    for j in i['runs']:
-        wr = {
-            'game': j['run']['game'],
-            'category': j['run']['category'],
-            'player': j['run']['players'][0]['id'],
-            'id': j['run']['id']
-        }
-        wr_database['data'].append(wr)
+for i in gamesList:
+    url = 'https://www.speedrun.com/api/v1/games/'+i+'/records?top=1&miscellaneous=yes&scope=full-game'
+    headers = {
+        'User-Agent': 'Is-there-a-new-wr/0.2'
+    }
+    httpRequestForGame = requests.get(url, headers=headers)
+    jsonData = json.loads(httpRequestForGame.text)
+    for j in jsonData['data']:
+        for k in j['runs']:
+            playerType = k['run']['players'][0]['rel']
+            if playerType == 'user':
+                wr = {
+                    'game': k['run']['game'],
+                    'category': k['run']['category'],
+                    'playerType': k['run']['players'][0]['rel'],
+                    'player': k['run']['players'][0]['id'],
+                    'id': k['run']['id']
+                }
+                wr_database['data'].append(wr)
+            elif playerType == 'guest':
+                wr = {
+                    'game': k['run']['game'],
+                    'category': k['run']['category'],
+                    'playerType': k['run']['players'][0]['rel'],
+                    'player': k['run']['players'][0]['name'],
+                    'id': k['run']['id']
+                }
+                wr_database['data'].append(wr)
 
 jsonWrDatabase = json.dumps(wr_database, indent = 4)
 with open("wrDatabase.json", "w") as outfile:
