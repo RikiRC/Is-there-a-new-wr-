225bd7e3ac20e04cf022b00bd2cc3715a8ec6b1c
RikiRC <47928112+RikiRC@users.noreply.github.com>
2021-02-16 16:38:35 +0100

    Update Twitch_checkForNewWR.py

--- Twitch_checkForNewWR.py
+++ Twitch_checkForNewWR.py
@@ -13,7 +13,7 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
     def __init__(self, username, client_id, token, channel):
         self.client_id = client_id
         self.token = token
-        self.channel = '#' + channel
+        self.channel = '#' + channel.lower()
 
         url = 'https://api.twitch.tv/kraken/users?login=' + channel
         headers = {'Client-ID': client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
